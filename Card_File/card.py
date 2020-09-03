b62d8693bc5cf80ef0d46c4e14d97ae13773e373
Patrick <patrickrene.mwimanzi@gmail.com>
2020-09-03 18:35:29 +0200

    files deleted

--- Card_File/card.py
+++
@@ -1,8 +0,0 @@
-class Card:
-    def __init__(self, color, value):
-        self.color = color
-        self.value = value
-
-    def __str__(self):
-        return ' \U0001f0a0  '.join((self.value, self.color))
-
